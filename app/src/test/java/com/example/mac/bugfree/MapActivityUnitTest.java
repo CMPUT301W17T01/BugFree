90613073e623f5ab46c726685b3116b827534eac
JohnDoeMask <heyuehuang@hotmail.com>
2017-02-24 17:34:48 -0700

    Add Unittest files for filter and map

---
+++ app/src/test/java/com/example/mac/bugfree/MapActivityUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.mac.bugfree;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by heyuehuang on 2017-02-22.
+ */
+
+public class MapActivityUnitTest extends ActivityInstrumentationTestCase2 {
+
+    public MapActivityUnitTest() {
+        super(MapActivity.class);
+    }
+
+ }
