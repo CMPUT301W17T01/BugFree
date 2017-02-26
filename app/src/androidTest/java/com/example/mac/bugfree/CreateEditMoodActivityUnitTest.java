158e446512fbae8812f681112bdebed11c19590f
YMChen95 <sylarchencn@gmail.com>
2017-02-26 01:28:57 -0700

    unit tests

--- app/src/androidTest/java/com/example/mac/bugfree/CreateEditMoodActivityUnitTest.java
+++ app/src/androidTest/java/com/example/mac/bugfree/CreateEditMoodActivityUnitTest.java
@@ -2,10 +2,8 @@ package com.example.mac.bugfree;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
-
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by mengyangchen on 2017-02-23.
@@ -15,6 +13,7 @@ public class CreateEditMoodActivityUnitTest extends ActivityInstrumentationTestC
     public CreateEditMoodActivityUnitTest(){
         super(CreateEditMoodActivity.class);
     }
+
     public void test_load_mood_list(){
         CreateEditMoodActivity mood= new CreateEditMoodActivity();
         User new_mood = new User();
@@ -26,6 +25,7 @@ public class CreateEditMoodActivityUnitTest extends ActivityInstrumentationTestC
         User new_mood = new User();
         assertFalse(mood.save_mood_list());
     }
+
     public void test_add_location(){
         CreateEditMoodActivity location= new CreateEditMoodActivity();
         User new_mood = new User();
