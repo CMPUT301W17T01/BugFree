4b8debd0028ea7542c0168d92ba6383ee65c761f
Raymundo1 <cquptcxl@gmail.com>
2017-02-25 21:13:15 -0700

    add MoodEventList

---
+++ app/src/main/java/com/example/mac/bugfree/MoodEventList.java
@@ -0,0 +1,59 @@
+package com.example.mac.bugfree;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by mac on 2017-02-25.
+ */
+
+public class MoodEventList {
+
+    private ArrayList<MoodEvent> moodEventArrayList;
+
+
+    public MoodEventList(ArrayList<MoodEvent> moodEventList) {
+        this.moodEventArrayList = moodEventList;
+    }
+
+    public MoodEventList() {
+        this.moodEventArrayList = new ArrayList<MoodEvent>();
+    }
+
+    public void addMoodEvent(MoodEvent moodEvent) {
+
+        if (this.hasMoodEvent(moodEvent)) {
+            throw new IllegalArgumentException("Already have same MoodEvent in list");
+        }
+
+        this.moodEventArrayList.add(moodEvent);
+    }
+
+    public boolean hasMoodEvent(MoodEvent moodEvent) {
+        return this.moodEventArrayList.contains(moodEvent);
+    }
+
+    public void deleteMoodEvent(MoodEvent moodEvent) {
+        this.moodEventArrayList.remove(moodEvent);
+    }
+
+    public MoodEvent getMoodEvent(int index) {
+        return this.moodEventArrayList.get(index);
+    }
+
+    public int getCount() {
+        return this.moodEventArrayList.size();
+    }
+
+    public void sortByDate() {
+        Collections.sort(moodEventArrayList, new Comparator<MoodEvent>() {
+            @Override
+            public int compare(MoodEvent o1, MoodEvent o2) {
+                return o1.getDateOfRecord().compareTo(o2.getDateOfRecord());
+            }
+        });
+    }
+
+}
