3d6bb9b29aa3efefed4f6dc5c3ff562e3c9e9d59
JohnDoeMask <heyuehuang@hotmail.com>
2017-02-25 23:50:49 -0700

    finish unit test

--- app/src/test/java/com/example/mac/bugfree/MapActivityUnitTest.java
+++ app/src/test/java/com/example/mac/bugfree/MapActivityUnitTest.java
@@ -1,22 +1,24 @@
 package com.example.mac.bugfree;
 
 import android.test.ActivityInstrumentationTestCase2;
-
+import android.test.ActivityInstrumentationTestCase2;
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 /**
  * Created by heyuehuang on 2017-02-22.
  */
 
-public class MapActivityUnitTest extends ActivityInstrumentationTestCase2 {
+public class MapActivityUnitTest  {
 
-    public MapActivityUnitTest() {
-        super(MapActivity.class);
-    }
-
+    @Test
     public void testShowPin(){
         MapActivity pin = new MapActivity();
         assertFalse(pin.showPin());
     }
 
+    @Test
     public void testShowDetail(){
         MapActivity detail = new MapActivity();
         assertFalse(detail.showDetail());
