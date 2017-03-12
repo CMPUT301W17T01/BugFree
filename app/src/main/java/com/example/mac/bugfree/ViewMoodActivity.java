3571805b14700131b6f2f39128941882d0a2dfb4
YMChen95 <sylarchencn@gmail.com>
2017-03-11 17:09:45 -0700

    update view

--- app/src/main/java/com/example/mac/bugfree/ViewMoodActivity.java
+++ app/src/main/java/com/example/mac/bugfree/ViewMoodActivity.java
@@ -16,7 +16,10 @@ import org.w3c.dom.Text;
 
 import com.google.gson.Gson;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * @author Mengyang Chen
@@ -26,22 +29,50 @@ public class ViewMoodActivity extends AppCompatActivity {
     private String mood_state;
     private String social_situation;
     private String reason;
-    private Date date;
+    private Date date =null;
+    private MoodEvent moodEvent;
+    GregorianCalendar dateOfRecord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_mood);
 
+        load_moodEvent();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_view);
         setSupportActionBar(toolbar);
 
         TextView moodState = (TextView) findViewById(R.id.moodState_textView);
         TextView socialSituation = (TextView) findViewById(R.id.socialSituation_textView);
         TextView reason = (TextView) findViewById(R.id.reason_textView);
-        TextView date = (TextView) findViewById(R.id.date_textView);
+        TextView date_text = (TextView) findViewById(R.id.date_textView);
         ImageView image = (ImageView) findViewById(R.id.imageView);
 
         image.setImageResource(R.drawable.picture_text);
+
+        moodState.setText(moodEvent.getMoodState().toString());
+        if(moodEvent.getSocialSituation()==null){
+            //do nothing
+        }
+        else {
+            socialSituation.setText(moodEvent.getSocialSituation().toString());
+        }
+        if(moodEvent.getTriggerText()==null){
+            //do nothing
+        }
+        else {
+            reason.setText(moodEvent.getTriggerText().toString());
+        }
+
+        dateOfRecord = moodEvent.getDateOfRecord();
+        //date.setText(dateOfRecord.toString());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S");
+        fmt.applyPattern("yyyy MM dd HH mm ss");
+        try {
+            date = fmt.parse(dateOfRecord.getTime().toString());
+        } catch (ParseException e) {
+            Log.i("error message", "");
+        }
+        date_text.setText(fmt.format(date));
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,8 +95,11 @@ public class ViewMoodActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean load_mood_list(){
-        return true;
+    public void load_moodEvent(){
+        SharedPreferences sharedPreferences =getSharedPreferences("viewMoodEvent", MODE_PRIVATE);
+        Gson gson =new Gson();
+        String json = sharedPreferences.getString("moodevent","");
+        moodEvent=gson.fromJson(json,MoodEvent.class);
     }
 
     protected void onStart(){
