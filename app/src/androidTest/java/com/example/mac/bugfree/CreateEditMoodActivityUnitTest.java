26d9e11d7e8c42548da930a871ee419a246ded94
Yipeng Zhou <yipeng6@ualberta.ca>
2017-03-12 20:01:04 -0600

    finished view and create test

--- app/src/androidTest/java/com/example/mac/bugfree/CreateEditMoodActivityUnitTest.java
+++ app/src/androidTest/java/com/example/mac/bugfree/CreateEditMoodActivityUnitTest.java
@@ -4,6 +4,7 @@ package com.example.mac.bugfree;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.robotium.solo.Solo;
@@ -25,8 +26,34 @@ public class CreateEditMoodActivityUnitTest  extends ActivityInstrumentationTest
     }
     public void testcheckbox(){
         solo.assertCurrentActivity("Wrong Activity", CreateEditMoodActivity.class);
+        View cb = solo.getView(R.id.current_time);
+        solo.clickOnView(cb);
+    }
 
+    public void testspinner1(){
+        solo.assertCurrentActivity("Wrong Activity", CreateEditMoodActivity.class);
+        View spnr = solo.getView(R.id.mood_state_spinner);
+        solo.clickOnView(spnr);
+    }
 
+    public void testspinner2(){
+        solo.assertCurrentActivity("Wrong Activity", CreateEditMoodActivity.class);
+        View spnr = solo.getView(R.id.social_situation);
+        solo.clickOnView(spnr);
+    }
+
+    public void testreason(){
+        solo.assertCurrentActivity("Wrong Activity", CreateEditMoodActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.create_edit_reason), "cool!");
+        assertTrue(solo.searchText("cool!"));
+    }
+
+    public void testchecktick(){
+        View ct = solo.getView(R.id.action_add_tick);
+        solo.pressSpinnerItem(0,1);
+        solo.pressSpinnerItem(1,1);
+        solo.enterText((EditText) solo.getView(R.id.create_edit_reason), "cool!");
+        solo.clickOnView(ct);
     }
 
 }
