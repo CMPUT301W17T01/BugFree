6a3700ffdd433e7cdd84cae90fa9c02062425ca9
JohnDoeMask <heyuehuang@hotmail.com>
2017-03-19 23:25:53 -0600

    add scale

--- app/src/main/java/com/example/mac/bugfree/activity/MapActivity.java
+++ app/src/main/java/com/example/mac/bugfree/activity/MapActivity.java
@@ -1,5 +1,6 @@
 package com.example.mac.bugfree.activity;
 
+import java.util.ArrayList;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mac.bugfree.R;
 
@@ -17,8 +19,18 @@ import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 
 public class MapActivity extends AppCompatActivity {
+    private MapView mOpenMapView;
+
+    ArrayList<OverlayItem> anotherOverlayItemArray;
+
+    MyLocationNewOverlay myLocationOverlay = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +43,27 @@ public class MapActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        mOpenMapView = (MapView) findViewById(R.id.map);
+        mOpenMapView.setTileSource(TileSourceFactory.MAPNIK);
 
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
+        mOpenMapView.setBuiltInZoomControls(true);
+        mOpenMapView.setMultiTouchControls(true);
 
-        IMapController mapController = map.getController();
+        IMapController mapController = mOpenMapView.getController();
         mapController.setZoom(9);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        GeoPoint startPoint = new GeoPoint(45.4, -75.666667);
         mapController.setCenter(startPoint);
 
+
+        //Add Scale Bar
+        ScaleBarOverlay myScaleBarOverlay = new ScaleBarOverlay(mOpenMapView);
+        mOpenMapView.getOverlays().add(myScaleBarOverlay);
+
+        //Add MyLocationOverlay
+        this.myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this) , mOpenMapView);
+        this.myLocationOverlay.enableMyLocation();
+        this.mOpenMapView.getOverlays().add(this.myLocationOverlay);
+        mOpenMapView.postInvalidate();
 
         ImageView add_tab = (ImageView) findViewById(R.id.add_tab_map);
         add_tab.setOnClickListener(new View.OnClickListener() {
@@ -62,9 +84,11 @@ public class MapActivity extends AppCompatActivity {
         });
     }
 
+
     public void onResume() {
         super.onResume();
         org.osmdroid.config.Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
     }
 
     public boolean showPin(){
